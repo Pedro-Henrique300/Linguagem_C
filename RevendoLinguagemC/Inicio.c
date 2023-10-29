03284e4d59607eee7b5c9a222853fa4071a59238
Pedro Henrique <pedrohenriquecamargo2019@gmail.com>
2023-10-29 18:08:34 -0300

    add linguagemc

---
+++ RevendoLinguagemC/Inicio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main()
+{
+	printf("\n\tOi, estou praticando linguagem C !\n\n");
+	
+	printf("\tTenho %d anos!\n\n", 18);
+	printf("\tO numero de pi eh: %f\n\n", 3.14159265);
+	printf("\tMas pode-se considerar: %.2f\n\n", 3.14159265);
+	printf("\tAmo estudar %s\n\n", "programacao");
+	printf("\tA inicial do meu nome eh: %c\n", 'P');
+	
+}
