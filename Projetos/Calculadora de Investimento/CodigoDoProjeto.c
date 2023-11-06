79ca6b50e08a1607f3ef0ecf253d0cb8ab0aeabb
Pedro Henrique <pedrohenriquecamargo2019@gmail.com>
2023-11-06 17:15:33 -0300

    up na calculadora

--- Projetos/Calculadora de Investimento/CodigoDoProjeto.c
+++ Projetos/Calculadora de Investimento/CodigoDoProjeto.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<locale.h>
+#include <math.h>
 
 /*
 C = Capital
@@ -7,29 +9,35 @@ t = Valor da taxa
 */
 
 
-main()
-{
+main(){
+	setlocale(LC_ALL, "Portuguese");
+	
 	float C, I, t;
-	int i;
+	int i, meses;
 	
 	printf("Informe o quanto de dinheiro vai ser investido: R$ ");
 	scanf("%f", &C);
+	fflush(stdin);
 	
-	printf("\nInforme a taxa de CDI atualmente ( sem o sinal de %% por favor ): ");
+	printf("\nInforme a taxa de CDI atualmente (sem o %%, e troque , por . por favor): ");
 	scanf("%f", &I);
+	fflush(stdin);
 	
+	printf("\nInforme o tempo em meses: ");
+	scanf("%d", &meses);
 		
 		t = 100 + I;
 		t /= 100; 
 		
 		
-	for(i=1; i<=12; i++){
+	for(i=1; i<=meses; i++){
+		
 		C *= t;
-		printf("\nNo mes %d apos o investimento voce estara com R$%.2f\n", i, C);
-	}
+		printf("\nNo %dº mês após o investimento você estará com R$%.2f\n", i, C);
+}
 	
-	
-	printf("\nPessione Enter para continuar...");
+	printf("\nPressione >Enter< para encerrar...");
+	getchar();
 	getchar();
 	
 	return 0;
